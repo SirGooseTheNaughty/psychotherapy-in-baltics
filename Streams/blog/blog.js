699768b9d437f350a3ceb1267e8f47d5fcf5f4b0
Sergey Vorobyev <syvorobyev@gmail.com>
2021-12-21 15:48:23 +0300

    blog date format

--- Streams/blog/blog.js
+++ Streams/blog/blog.js
@@ -134,10 +134,15 @@ const Blog = {
                         }
                     }
                 });
+                const date = post.date.split(' ')[0];
+                const [ year, month, day ] = date.split('-');
                 return {
                     title: post.title,
                     img: post.image || null,
-                    date: post.date.split(' ')[0],
+                    date,
+                    year,
+                    month,
+                    day,
                     time: post.descr,
                     content: post.text,
                     categories: categories,
@@ -179,7 +184,7 @@ const Post = {
                     <div class="card__category">{{ data.category || '' }}</div>
                     <h4 class="card__info-title">{{ data.title }}</h4>
                     <div class="card__info-desc">
-                        <p class="card__info-desc__date">{{ data.date }}</p>
+                        <p class="card__info-desc__date">{{ date }}</p>
                         <p v-if="data.time" class="card__info-desc__time"><span>${timeConsumationIcon}</span><span>{{ data.time }}</span></p>
                     </div>
                 </div>
@@ -193,6 +198,9 @@ const Post = {
         },
         isMobile: function() {
             return document.documentElement.offsetWidth < 1200;
+        },
+        date: function() {
+            return `${this.day} ${this.translator.getTranslation(['common', 'months', this.month])}, ${this.year}`;
         }
     }
 };
