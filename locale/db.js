af816bb086c1f37e8e362ec68e14241d5f69a4ec
Sergey Vorobyev <syvorobyev@gmail.com>
2023-04-07 20:30:41 +0400

    add translations

--- locale/db.js
+++ locale/db.js
@@ -5,12 +5,14 @@ const localeDb = {
             en: 'Programs curators',
             lv: 'Programmas kuratori',
             et: 'Programmide kuraatorid',
+            lt: 'Programos kuratoriai',
         },
         experts: {
             ru: 'Преподаватели',
             en: 'Professors',
             lv: 'Pasniedzēji',
             et: 'õpetajad',
+            lt: 'Profesoriai',
         }
     },
     blog: {
@@ -19,6 +21,7 @@ const localeDb = {
             en: "Couldn't find any posts by these parameters",
             lv: 'Šiem parametriem nav atrasta neviena publikācija',
             et: 'Nende parameetrite kohta ei leitud ühtegi väljaannet',
+            lt: 'Nepavyko rasti jokių įrašų pagal šiuos parametrus',
         }
     },
     news: {
@@ -27,12 +30,14 @@ const localeDb = {
             en: 'Upcoming events',
             lv: 'Tuvākie pasākumi',
             et: 'Üritused',
+            lt: 'Artimiausi renginiai',
         },
         events: {
             ru: 'События',
             en: 'Events',
             lv: 'Notikumi',
             et: 'Sündmused',
+            lt: 'Renginiai',
         },
     },
     noPostsSearch: {
@@ -40,30 +45,35 @@ const localeDb = {
         en: "Couldn't find any posts by this query",
         lv: 'Šim vaicājumam nav atrasta neviena publikācija',
         et: 'Selle päringu jaoks ei leitud ühtegi väljaannet',
+        lt: 'Nepavyko rasti jokių įrašų pagal šią užklausą',
     },
     startTyping: {
         ru: 'Начните печатать для поиска',
         en: "Start typing to search",
         lv: 'Sāciet rakstīt, lai atrastu meklēto',
         et: 'Alusta otsimist',
+        lt: 'Norėdami ieškoti, pradėkite rašyti',
     },
     enterYourQuery: {
         ru: 'Введите свой запрос',
         en: "Enter your query",
         lv: 'Ievadiet pieprasījumu',
         et: 'Sisesta oma päring',
+        lt: 'Įveskite savo užklausą',
     },
     more: {
         ru: 'Подробнее',
         en: 'See more',
         lv: 'Vairāk',
         et: 'Rohkem ',
+        lt: 'Daugiau',
     },
     seemore: {
         ru: 'смотреть еще',
         en: 'see more',
         lv: 'redzēt vairāk',
         et: 'näe rohkem',
+        lt: 'Žiūrėti daugiau',
     },
     nodata: {
         lang: {
@@ -71,24 +81,28 @@ const localeDb = {
             en: 'This part is not available in English for now. It will be added as soon as possible.<br>Sorry for inconvenience!',
             lv: 'Šī daļa vēl nav pieejama krievu valodā. Tas tiks pievienots, cik drīz vien iespējams. <br> Atvainojiet par sagādātajām neērtībām!',
             et: 'See osa pole veel eesti keeles saadaval. See lisatakse esimesel võimalusel.<br>Vabandame ebamugavuste pärast!',
+            lt: 'Šios dalies lietuvių kalba kol kas nėra. Jis bus pridėtas kuo greičiau.<br>Atsiprašome už nepatogumus!',
         },
         lv: {
             ru: 'На данный момент предстоящих событий в Латвии нет.',
             en: 'There are currently no upcoming events in Latvia.',
             lv: 'Šobrīd Latvijā nav gaidāmu pasākumu.',
             et: 'Lätis ei ole hetkel eelseisvaid üritusi.',
+            lt: 'Šiuo metu Latvijoje nėra jokių būsimų renginių.',
         },
         et: {
             ru: 'На данный момент предстоящих событий в Эстонии нет.',
             en: 'There are currently no upcoming events in Estonia.',
             lv: 'Igaunijā pašlaik nav gaidāmu notikumu.',
             et: 'Eestis hetkel tulemas üritusi ei ole.',
+            lt: 'Šiuo metu Estijoje nėra jokių būsimų renginių.',
         },
         lt: {
             ru: 'На данный момент предстоящих событий в Литве нет.',
             en: 'There are currently no upcoming events in Lithuania.',
             lv: 'Lietuvā pašlaik nav gaidāmu notikumu.',
             et: 'Leedus ei ole hetkel eelseisvaid üritusi.',
+            lt: 'Šiuo metu Lietuvoje artėjančių renginių nėra.',
         },
     }
 };
